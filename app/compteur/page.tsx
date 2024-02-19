bb1504cb7094520947dec8998e5ab6fdeb7066f5
Denys Koshevko <dkoshevko@gmail.com>
2024-02-19 19:19:23 +0100

    compteur

--- app/compteur/page.tsx
+++ app/compteur/page.tsx
@@ -1,7 +1,43 @@
+"use client";
+
+import { useState } from "react";
+
+
+const countButtonStyles: string = "w-36 h-36 rounded-full border-8 border-black font-bold";
+
+
 export default function Compteur() {
-    return(
-        <main>
-            Compteur
-        </main>
-    )
+  const [count, setCount] = useState<number>(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  const resetCount = () => {
+    setCount(0);
+  };
+
+  return (
+    <main>
+      <div className="flex flex-col justify-center items-center gap-10">
+        <span className="border-4 border-black rounded-full w-52 py-3 text-center text-6xl">
+          {count}
+        </span>
+        <div className="flex justify-center gap-10">
+          <button
+            className={`${countButtonStyles} bg-red-800`}
+            onClick={resetCount}
+          >
+            Remise à zéro
+          </button>
+          <button
+            className={`${countButtonStyles} bg-green-600 text-4xl`}
+            onClick={incrementCount}
+          >
+            +
+          </button>
+        </div>
+      </div>
+    </main>
+  );
 }
